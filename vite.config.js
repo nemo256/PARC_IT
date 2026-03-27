ca4b8f17173bc7d398bdf076c88b14cb8d49ab4b
Amine Neggazi <neggazimedlamine@gmail.com>
2026-03-27 20:44:40 +0100

    update deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/parc_it/', // This must match your repo name exactly
+  build: {
+    outDir: 'docs', // This tells Vite to build into /docs instead of /dist
+  },
   plugins: [react()],
 })
